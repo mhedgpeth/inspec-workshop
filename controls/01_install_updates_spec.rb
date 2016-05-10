bab0ea563a1b60e10dff16a184076c5a45d8079e
Michael Hedgpeth <michael.hedgpeth@ncr.com>
2016-05-10 14:54:29 -0500

    completed 1.2.2

--- controls/01_install_updates_spec.rb
+++ controls/01_install_updates_spec.rb
@@ -4,8 +4,18 @@ title 'Install Updates, Patches and Additional Security Software'
 control "cis-centos-1-2-1-level-1" do
   impact 1.0
   title "Verify CentOS GPG Key is Installed"
-  desc "An optional description..."
+  desc "CentOS cryptographically signs updates with a GPG key to verify that they are valid."
   describe command('rpm -q --queryformat "%{SUMMARY}\n" gpg-pubkey') do
     its('stdout') { should match /CentOS 6 Official Signing Key/ }
+  end
+end
+
+control "cis-centos-1-2-2-level-1" do
+  impact 1.0
+  title "Veirfy that gpgcheck is Globally Activated"
+  desc "The gpgcheck option, found in the main section of the /etc/yum.conf file determines if an
+RPM package's signature is always checked prior to its installation."
+  describe file('/etc/yum.conf') do
+    its('content') { should match /gpgcheck=1/ }
   end
 end
